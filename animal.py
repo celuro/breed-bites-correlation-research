83e127df6145e28a603527500168917f4974c881
celuro <144652524+celuro@users.noreply.github.com>
2024-04-11 14:34:57 -0400

    Update animal.py

--- animal.py
+++ animal.py
@@ -1,28 +1,27 @@
-# Research Question: What breed is more likely to have rabies?
-# Variables: Breed (independent), Rabies (dependent)
-# Hypothesis: There is a relationship between pit bulls having a higher chance of rabies compared to labrador retrievers due to a higher rate of animal abuse.
-# Ensure breed is (PIT BULL, AMER. BULL DOG, GOLD RETRIEVER, LABRADOR RETRIV)
-# Testable Prediction: If my hypothesis is correct, I expect animal abuse to have a higher correlation of rabies.
-# Experient Model: Factorized design (2x2)
+# Research Question: What breed is more likely to have bite the body?
+# Variables: Breed (independent), BittenIDDesc (dependent)
+# Hypothesis: There is a relationship between pit bulls having a higher chance of biting compared to labrador retrievers due to a higher rate of animal abuse.
+# Ensure breed is (PIT BULL, AMER. BULL DOG) and (LABRADOR RETRIEVER, GOLDEN RETRIEVER)
+# Testable Prediction: If my hypothesis is correct, I expect animal abuse to have a higher correlation of bites.
 
 # Importing the pandas library to use its functions
 import pandas as pd
 df = pd.read_csv('Health_AnimalBites.csv')
 
-# Handling Missing Values
-# Delete rows that aren't dogs and dont have a breed
+#Handling Missing Values
+#Delete rows that dont aren't dogs, dont have a breed and don't have a bitten id desc
 not_dog = df[df['SpeciesIDDesc'] != "DOG"].index
 not_breed = df[df['BreedIDDesc'].isna()].index
+not_bitten = df[df['WhereBittenIDDesc'].isna()].index
 
-# Deleting the rows based on variables
-rows_to_drop = not_dog.union(not_breed)
+rows_to_drop = not_dog.union(not_breed).union(not_bitten)
 df.drop(rows_to_drop, inplace=True)
 
-# I want the data to be unique values
+# Handle duplicate data
 df.drop_duplicates(inplace=True)
 
-# One-Hot Label Encoding due to categories not needing an order
-df_encoded = pd.get_dummies(df, columns=['SpeciesIDDesc', 'BreedIDDesc', 'ResultsIDDesc'])
+#Label Encoding
+df_encoded = pd.get_dummies(df, columns=['SpeciesIDDesc', 'BreedIDDesc', 'WhereBittenIDDesc'])
+df_encoded.to_csv('new_data.csv', index=False)
 
-# Saving the cleaned and encoded data frame to a csv for future use
-df_encoded.to_csv('fixed_data.csv', index=False)
+
